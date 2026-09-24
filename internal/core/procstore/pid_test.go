8bb9fb1bb94cbcab84f534025d2f64725bb197bf
Tiago Oliveira <tfgoliveira@gmail.com>
2026-09-24 22:05:41 +0100

    test(daemon): cover process identity matching

---
+++ internal/core/procstore/pid_test.go
@@ -0,0 +1,24 @@
+package procstore
+
+import (
+	"os"
+	"testing"
+	"time"
+)
+
+func TestProcessMatchesRejectsClearlyDifferentStartTime(t *testing.T) {
+	pid := os.Getpid()
+	if !PidAlive(pid) {
+		t.Fatalf("current process PID %d should be alive", pid)
+	}
+	if ProcessMatches(pid, time.Now().Add(-2*time.Hour), "") {
+		t.Fatalf("PID %d matched a start time from two hours ago", pid)
+	}
+}
+
+func TestProcessMatchesCurrentProcess(t *testing.T) {
+	pid := os.Getpid()
+	if !ProcessMatches(pid, time.Now(), "") {
+		t.Fatalf("current process PID %d did not match recent start metadata", pid)
+	}
+}
